71859fd367cc7d10a53299723f8bd9c3db77b319
mac <anton.roienko@openvpn.net>
2022-09-21 21:08:56 +0300

    update feature

--- bot/src/features/botAdmin.feature.ts
+++ bot/src/features/botAdmin.feature.ts
@@ -4,25 +4,50 @@ import { userDao } from "@bot/dao";
 import { config } from "@bot/config";
 import { logHandle } from "@bot/helpers/logging";
 import { router } from "@bot/middlewares";
+import { usersService } from "@bot/services";
 
 export const feature = router
-  .route("wallet")
+  .route("admin")
   .filter(isUserId(config.BOT_ADMIN_USER_ID));
 
-feature.command("stats", logHandle("handle /stats"), async (ctx) => {
-  await ctx.replyWithChatAction("typing");
+feature
+  .filter(isUserId(config.BOT_ADMIN_USER_ID))
+  .command("stats", logHandle("handle /stats"), async (ctx) => {
+    await ctx.replyWithChatAction("typing");
 
-  const usersCount = await userDao.count();
+    const usersCount = await userDao.count();
 
-  const stats = `Users count: ${usersCount}`;
+    const stats = `Users count: ${usersCount}`;
 
-  return ctx.reply(stats);
-});
+    return ctx.reply(stats);
+  });
 
-feature.command(
-  "setcommands",
-  logHandle("handle /setcommands"),
-  async (ctx) => {
+feature
+  .filter(isUserId(config.BOT_ADMIN_USER_ID))
+  .command("updates", logHandle("handle updates"), async (ctx) => {
+    ctx.session.step = "admin";
+    return ctx.reply("Write update message");
+  });
+
+feature
+  .filter((ctx) => {
+    const isAdmin = isUserId(config.BOT_ADMIN_USER_ID);
+    return isAdmin(ctx) && ctx.session.step === "admin";
+  })
+  .on("message:text", async (ctx) => {
+    const message = ctx.message.text || "";
+
+    const { getAllUser } = usersService();
+    const users = await getAllUser();
+
+    for (const user of users) {
+      await ctx.api.sendMessage(Number(user.telegramId), message);
+    }
+  });
+
+feature
+  .filter(isUserId(config.BOT_ADMIN_USER_ID))
+  .command("setcommands", logHandle("handle /setcommands"), async (ctx) => {
     await ctx.replyWithChatAction("typing");
 
     // set private chat admin commands
@@ -46,5 +71,4 @@ feature.command(
     );
 
     return ctx.reply("Commands updated");
-  }
-);
+  });
