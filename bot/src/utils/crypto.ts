ce28bdc1b21c91b1aca49a192e5afbf25e6bc7a3
mac <anton.roienko@openvpn.net>
2022-09-22 20:38:30 +0300

    test

--- bot/src/utils/crypto.ts
+++ bot/src/utils/crypto.ts
@@ -23,17 +23,13 @@ export function decrypt(
   },
   key: string
 ) {
-  console.log(444, key);
   const hash = getPasswordHash(key);
   const iv = Buffer.from(text.iv, "hex");
   const encryptedText = Buffer.from(text.encryptedData, "hex");
-  console.log(555, hash, encryptedText);
   const decipher = createDecipheriv(algorithm, hash, iv);
-  console.log(666, decipher);
+  decipher.setAutoPadding(false);
   let decrypted = decipher.update(encryptedText);
-  console.log(777, decrypted);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
-  console.log(888, decrypted);
 
   return decrypted.toString();
 }
